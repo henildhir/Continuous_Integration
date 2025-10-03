252c5493071737553f3f5b5f94337b6c99f206e2
henildhir <dhirajlal.henil297@gmail.com>
2025-10-03 17:32:55 +0100

    added third number

--- Unit_Tests.py
+++ Unit_Tests.py
@@ -6,10 +6,10 @@ class my_unit_tests(unittest.TestCase):
     def test_add(self):
 
         # test adding negative integers
-        self.assertEqual(add_numbers(4,5),9)
+        self.assertEqual(add_numbers(4,5,3),12)
 
         # test adding floats"
-        self.assertEqual(add_numbers(3,2),5)
+        self.assertEqual(add_numbers(3,2,1),6)
 
 # run the tests
 if __name__ == "__main__":
