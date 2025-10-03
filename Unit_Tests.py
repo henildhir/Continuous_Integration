ff080332d995dd74f2e989298c5c48ecd6df2ec8
henildhir <dhirajlal.henil297@gmail.com>
2025-10-03 15:19:57 +0100

    added unit test

--- Unit_Tests.py
+++ Unit_Tests.py
@@ -1,15 +1,15 @@
 import unittest
-from my_function import multiply
+from my_function import add_numbers
 
 # define the unit tests
 class my_unit_tests(unittest.TestCase):
     def test_add(self):
 
         # test adding negative integers
-        self.assertEqual(multiply(4,5),20)
+        self.assertEqual(add_numbers(4,5),9)
 
         # test adding floats"
-        self.assertEqual(multiply(3,2),7)
+        self.assertEqual(add_numbers(3,2),5)
 
 # run the tests
 if __name__ == "__main__":
