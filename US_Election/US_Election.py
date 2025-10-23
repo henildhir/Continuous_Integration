42e55b1428b879534c6fe40f64dad934d9684cad
henildhir <dhirajlal.henil297@gmail.com>
2025-10-23 15:01:14 +0100

    changes to election graph

--- US_Election/US_Election.py
+++ US_Election/US_Election.py
@@ -1,21 +1,22 @@
+#imported all functions needed for this function
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#assigned a dataframe variable to read csv file with seperators as ;
 US_Election_df = pd.read_csv("US_Election/US-2016-primary.csv",sep=';')
-candidate_name='Donald Trump'
-state=input("Enter a state you want to look at: ")
-candidate_df= US_Election_df[US_Election_df['candidate']==candidate_name]
-state_df=candidate_df[candidate_df['state'].str.lower()==state.lower()]
 
-state_df['fraction_votes']=pd.to_numeric(state_df['fraction_votes'],errors='coerce')
+#asks for a candidate name and filtered the dataframe to show only the candidate chosen
+candidate_name=input("Enter a candidate: ")
+candidate_df= US_Election_df[US_Election_df['candidate'].str.lower()==candidate_name.lower()]
 
+#groups the candidate dataframe by state first then adds all the fraction votes together
+state_votes = candidate_df.groupby('state')['fraction_votes'].sum()
 
+#this plots a bar graph of state on the x axis and fraction of votes on the y axis with the candidate name being at the title
 plt.figure(figsize=(8,5))
-
-plt.hist(state_df['fraction_votes'].dropna(),bins=30)
-
-plt.xlabel('Fraction of Votes')
-plt.ylabel('Number of Counties')
+state_votes.plot(kind='bar')
+plt.xlabel('State')
+plt.ylabel('fraction of votes')
 plt.title(f'Distribution of fraction of votes for {candidate_name}')
 plt.tight_layout()
 plt.show()
